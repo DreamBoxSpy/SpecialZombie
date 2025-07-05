6acd9be65499c7bb85969d24102dbc1a8b0c9261
DreamBoxSpy <156294353+DreamBoxSpy@users.noreply.github.com>
2025-07-05 14:11:09 +0800

    Update

--- SpecialZombie/SpecialZombieEntity.cs
+++ SpecialZombie/SpecialZombieEntity.cs
@@ -1,82 +1,116 @@
 ﻿using dc;
 using dc.en.mob;
 using dc.pr;
+using ModCore.Serialization;
+using ModCore.Storage;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpecialZombie
 {
-    internal class SpecialZombieEntity : Entity
+    internal class SpecialZombieEntity : Entity, 
+        IHxbitSerializable<SpecialZombieEntity.Data>,
+        IHxbitSerializeCallback
     {
-        public Zombie zombie;
-        public int maxLife;
-        public int state = 0;
+        private class Data
+        {
+            public Defender defender = null!;
+            public Zombie zombie = null!;
+            public int maxLife;
+            public int state = 0;
+        }
+
+        private Data data = new();
+
         public SpecialZombieEntity(Zombie target, Level lvl, int x, int y) : base(lvl, x, y)
         {
             init();
-            zombie = target;
+            data.zombie = target;
         }
 
         public override void preUpdate()
         {
-            var life = zombie.life;
-            if(state > 3 || 
+            var life = data.zombie.life;
+            if(data.state > 3 || 
                 life == 0)
             {
                 return;
             }
-            if(state == 0 && zombie.elite)
+            if(data.state == 0 && data.zombie.elite)
             {
                 return;
             }
-            if (life > maxLife)
+            if (life > data.maxLife)
             {
-                maxLife = life;
+                data.maxLife = life;
             }
-            if(life < maxLife / 2)
+            if(life < data.maxLife / 2)
             {
                 //Angry
-                zombie.initLife(maxLife * 1.5f, maxLife * 2);
+                data.zombie.initLife(data.maxLife * 1.5f, data.maxLife * 2);
             }
             else
             {
                 return;
             }
-            if (state == 0)
+            if (data.state == 0)
             {
-                zombie.sprScaleX *= 1.25f;
-                zombie.sprScaleY *= 1.25f;
-                zombie.setOutlineColor(Utils.GetColor(80, 20, 20));
-                _level.fx.dirtExplosion(zombie.cx, zombie.cy, 4, 0, Utils.GetColor(80, 20, 20), default);
+                data.zombie.sprScaleX *= 1.25f;
+                data.zombie.sprScaleY *= 1.25f;
+                data.zombie.setOutlineColor(Utils.GetColor(80, 20, 20));
+                _level.fx.dirtExplosion(data.zombie.cx, data.zombie.cy, 4, 0, Utils.GetColor(80, 20, 20), default);
             }
-            else if(state == 1)
+            else if(data.state == 1)
             {
-                var fz = new Defender(_level, zombie.cx, zombie.cy, 1, 1);
+                var fz = new Defender(_level, data.zombie.cx, data.zombie.cy, 1, 1);
                 fz.init();
-                fz.initLife(maxLife / 2, maxLife);
+                fz.initLife(data.maxLife / 2, data.maxLife);
             }
-            else if (state == 2)
+            else if (data.state == 2)
             {
-                zombie.setElite(true);
-                _level.fx.boneSmoke(zombie.cx, zombie.cy, Utils.GetColor(10, 10, 100));
+                data.zombie.setElite(true);
+                _level.fx.boneSmoke(data.zombie.cx, data.zombie.cy, Utils.GetColor(10, 10, 100));
             }
-            else if (state == 3)
+            else if (data.state == 3)
             {
-                zombie.sprScaleX *= 1.25f;
-                zombie.sprScaleY *= 1.25f;
-                zombie.elite = false;
-                zombie.setElite(false);
-                _level.fx.boneSmoke(zombie.cx, zombie.cy, Utils.GetColor(10, 10, 100));
+                data.zombie.sprScaleX *= 1.25f;
+                data.zombie.elite = false;
+                data.zombie.sprScaleY *= 1.25f;
+                data.zombie.setElite(false);
+                _level.fx.boneSmoke(data.zombie.cx, data.zombie.cy, Utils.GetColor(10, 10, 100));
 
-                var fz = new Defender(_level, zombie.cx, zombie.cy, 1, 1);
+                var fz = new Defender(_level, data.zombie.cx, data.zombie.cy, 1, 1);
                 fz.init();
-                fz.initLife(maxLife / 3, maxLife / 2);
+                fz.initLife(data.maxLife / 3, data.maxLife / 2);
                 fz.setElite(false);
+                data.defender = fz;
             }
-            state++;
+            data.state++;
+        }
+
+        Data IHxbitSerializable<Data>.GetData()
+        {
+            return data;
+        }
+
+        void IHxbitSerializable<Data>.SetData(Data data)
+        {
+            this.data = data;
+        }
+
+        void IHxbitSerializeCallback.OnBeforeSerializing()
+        {
+            
+        }
+
+        void IHxbitSerializeCallback.OnAfterDeserializing()
+        {
+            Debug.Assert(data != null);
         }
     }
 }
--- SpecialZombie/SpecialZombieMod.cs
+++ SpecialZombie/SpecialZombieMod.cs
@@ -6,16 +6,29 @@ using HaxeProxy.Runtime;
 using ModCore.Events.Interfaces.Game;
 using ModCore.Mods;
 using ModCore.Modules;
+using ModCore.Storage;
 
 namespace SpecialZombie
 {
     public class SpecialZombieMod(ModInfo info) : ModBase(info)
     {
+        public readonly static SaveData<SData> save = new("SpecialZombieMod");
+        public class SData
+        {
+            public int couting;
+        }
         public override void Initialize()
         {
             Logger.Information("Hello, World!");
 
+            Hook_Mob.onDie += Hook_Mob_onDie;
             Hook_Mob.init += Hook_Mob_init;
+        }
+
+        private void Hook_Mob_onDie(Hook_Mob.orig_onDie orig, Mob self)
+        {
+            Logger.Information("Mob Die: {x}", self);
+            orig(self);
         }
 
         private void Hook_Mob_init(Hook_Mob.orig_init orig, Mob self)
@@ -23,7 +36,7 @@ namespace SpecialZombie
             orig(self);
             if(self is Zombie zombie)
             {
-                Logger.Information("Create zombie: {x}", self);
+                Logger.Information("Create zombie: {x} {a}", self, save.Value.couting++);
                 var sze = new SpecialZombieEntity(zombie, self._level, self.cx, self.cy);
             }
             
